138c1618e756febba9d1c81a3570566f44de08d5
ManiacTwister <git@s7t.de>
2012-09-09 23:26:25 +0200

    irclog

--- modules/python/scripts/logirc.py
+++ modules/python/scripts/logirc.py
@@ -88,7 +88,7 @@ class ircclient:
         i=0
         while(client.state != "offline"):
             i=i+1
-            logger.debug("PRIVMSG %s :%s\r\n" % (client.channel, i))
+            logger.debug("PRIVMSG %s :%s" % (client.channel, i))
             client.sendSocket("PRIVMSG %s :%s\r\n" % (client.channel, i))
             response = self.recvSocket()
             '''if "!" in response and ":" in response[response.index(":") + 1:]:
