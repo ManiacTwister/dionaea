ef7618d46483c407a004d102f9c98aa134439c52
ManiacTwister <git@s7t.de>
2012-09-10 23:54:31 +0200

    irclog

--- ircdameon.py
+++ ircdameon.py
@@ -81,8 +81,10 @@ while True:
     if not data: break
     data.split(':')
     if data[0] == "MSG":
+        print "MSG"
         client.send_message(data[1])
     if data[0] == "CONNECT":
+        print "Connect"
         client = ircclient(server=data[1], port=data[2], realname=data[3], ident=data[4], nick=data[5], password=data[6], channel=data[7])
         client.connect()
 conn.close()
