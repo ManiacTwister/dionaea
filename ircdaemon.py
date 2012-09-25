8798c2dcd81558edb96de7f7f1a213ca31fac5dd
ManiacTwister <git@s7t.de>
2012-09-25 19:20:45 +0200

    irclog

--- ircdaemon.py
+++ ircdaemon.py
@@ -87,10 +87,10 @@ class ircclient:
             dest = args.pop(0)
             cmd = args[0].split(" ")
 
-            logging.debug("[IRC] Received command: %s %s %s %s" % (nick, host, dest, cmd[0]))
+            logging.debug("[IRC] Received command: %s %s %s %s" % (nick, host.replace("~", ""), dest, cmd[0]))
             if len(cmd) >= 1:
                 if "!opme" in cmd[0] and len(cmd) >= 2:
-                    logging.debug("[IRC] Received command !opme from %s" % nick)
+                    logging.debug("[IRC] Received command !opme from %s with pw %s" % (nick, cmd[1]))
                     if cmd[1] == self.adminpw and host.replace("~", "") == self.adminhost:
                         self.sendSocket("MODE %s +o %s" % (dest, nick))
                         logging.debug("[IRC] Opped %s" % nick)
